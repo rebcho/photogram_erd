b2e20825907fb2227f22a87dde37118854e76012
Raghu Betina <raghu@firstdraft.com>
2018-01-07 00:46:05 +0000

    3rd draft

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,17 @@
 class Photo < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   # Direct associations
+
+  has_many   :comments,
+             :dependent => :destroy
 
   has_many   :likes,
              :dependent => :destroy
 
   # Indirect associations
 
-  has_many   :likers,
+  has_many   :fans,
              :through => :likes,
              :source => :user
 
