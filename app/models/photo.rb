1de8ca48d7edd75e33e381afb729c49f25dc75dd
Raghu Betina <raghu@firstdraft.com>
2018-01-07 00:17:48 +0000

    Indirect association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,10 @@ class Photo < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :likers,
+             :through => :likes,
+             :source => :user
+
   # Validations
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :liked_photos,
+             :through => :likes,
+             :source => :photo
+
   # Validations
 
 end
